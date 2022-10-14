9a19c859f19f6386fc1590a8b22eb19fee46fda5
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-10-14 13:19:08 -0400

    added create root

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import MainView from "./components/main-view/main-view";
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
@@ -16,4 +16,5 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName("app-container")[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+const root = createRoot(container);
+root.render(React.createElement(MyFlixApplication));
