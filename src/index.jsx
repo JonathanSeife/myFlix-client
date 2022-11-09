84b85733ba296cad6e7366d5962848c75b99d64f
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-11-09 10:23:18 -0500

    index update

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+<<<<<<< Updated upstream
 import { MainView } from "./components/main-view/main-view";
+=======
+import Container from "react-bootstrap/Container";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import moviesApp from "./reducers/reducers";
+
+import MainView from "./components/main-view/main-view";
+>>>>>>> Stashed changes
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
+<<<<<<< Updated upstream
     return <MainView />;
+=======
+    return (
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
+    );
+>>>>>>> Stashed changes
   }
 }
 
