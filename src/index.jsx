283c19efd851098b6e10343ef6988f15aba9bf22
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-11-14 11:22:40 -0500

    edited background

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ class MyFlixApplication extends React.Component {
     return (
       <Provider store={store}>
         <div className="screen">
-          <Container>
+          <Container fluid>
             <MainView />
           </Container>
         </div>
